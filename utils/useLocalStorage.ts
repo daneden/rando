bce3dbf4c6c841ccaef7c7dc890410650e54e490
Daniel Eden <dan.eden@me.com>
2021-03-17 20:14:24 +0000

    More logging improvements

--- utils/useLocalStorage.ts
+++ utils/useLocalStorage.ts
@@ -14,7 +14,7 @@ export default function useLocalStorage<T>(
       return item ? JSON.parse(item) : initialValue
     } catch (error) {
       // If error also return initialValue
-      console.log(error)
+      console.error(error)
       return initialValue
     }
   })
