f737a6401cc903867e7982c2a8272f25f9613463
Daniel Eden <dan.eden@me.com>
2021-03-17 11:28:42 +0000

    Add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import dynamic from "next/dynamic"
 import Layout from "../components/Layout"
+import Head from "next/head"
 
 const ItemList = dynamic(() => import("../components/ItemList"), { ssr: false })
 
 export default function HomePage() {
   return (
     <Layout>
+      <Head>
+        <title>Rando</title>
+      </Head>
       <ItemList />
     </Layout>
   )
